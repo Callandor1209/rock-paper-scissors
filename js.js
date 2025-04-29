809ee099469c3bea6faf6f079e5af90ca00cef76
Callandor1209 <vealeclara@gmail.com>
2025-04-29 12:13:46 -0400

    same as last

--- js.js
+++ js.js
@@ -101,8 +101,7 @@ function playRound(){
 
 }
 function reset1(){
-    const output = document.getElementById("output");
-    output.textContent.reset();
+    const output = document.getElementById("output").reset();
     var i = 1;
 }
 function addToConsole(message){
